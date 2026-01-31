3cf62a6896e632813339c553950f5f33c81e124b
Lncvrt <lncvrtreal@gmail.com>
2026-01-31 15:27:34 -0700

    Add support for loading 26.1 or higher savefile

--- database/loadAccount.php
+++ database/loadAccount.php
@@ -55,6 +55,43 @@ $savedata = json_decode($row2['save_data'], true);
 $savedata['account']['id'] = $id;
 $savedata['account']['name'] = $row['username'];
 $savedata['account']['session'] = $token;
+if ($savedata['version'] !== '0') {
+    $savedata['version'] = '0';
+    if (isset($savedata["bird"]["customIcon"]["purchased"]))
+    {
+        $purchased = $savedata["bird"]["customIcon"]["purchased"];
+        unset($savedata["bird"]["customIcon"]["purchased"]);
+        $data = [];
+        foreach ($purchased as $icon) {
+            $stmt2 = $conn1->prepare("SELECT userId, data, price, name FROM marketplaceicons WHERE id = ?");
+            $stmt2->bind_param("s", $icon);
+            $stmt2->execute();
+            $result3 = $stmt2->get_result();
+            $stmt2->close();
+            if ($result3->num_rows != 1) continue;
+            $row3 = $result3->fetch_assoc();
+            $uid = $row3['userId'];
+
+            $stmt3 = $conn0->prepare("SELECT username FROM users WHERE id = ?");
+            $stmt3->bind_param("i", $uid);
+            $stmt3->execute();
+            $result4 = $stmt3->get_result();
+            $stmt3->close();
+            if ($result4->num_rows != 1) continue;
+            $row4 = $result4->fetch_assoc();
+
+            $data[] = [
+                "username" => $row4["username"],
+                "userid" => $row3["userId"],
+                "data" => $row3["data"],
+                "uuid" => $icon,
+                "price" => $row3["price"],
+                "name" => base64_decode($row3["name"])
+            ];
+        }
+        $savedata["bird"]["customIcon"]["data"] = $data;
+    }
+}
 echo encrypt(json_encode([
     "success" => true,
     "data" => $savedata
